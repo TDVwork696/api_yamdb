785dd3b56badf6ecb907aa6b3157a6ad63b61a70
Ткачёв Даниил <tdvwork696@gmail.com>
2023-11-20 15:31:09 +0300

    Была произведена работа над Отзывами и Комментариями, а именно создана модель, сериализатор, вьюсет и обработчик запросов в урлс

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 # from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Categories, Genres, Titles
+from reviews.models import Categories, Genres, Titles, Reviews, Comments
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
@@ -40,3 +41,41 @@ class TitlesSerializer(serializers.ModelSerializer):
                 'Год должен быть не раньше 1970 и не позже 2023'
             )
         return value
+
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Reviews"""
+    score = serializers.IntegerField(min_value=1, max_value=10)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Reviews
+        fields = ('id', 'text', 'author', 'score', 'created')
+        read_only_fields = ('pub_date',)
+
+    def validate_resending(self, data):
+        if self.context['request'].method != 'POST':
+            return data
+        title = get_object_or_404(
+            Titles,
+            id=self.context['request'].parser_context['kwargs']['title_id']
+        )
+        if Reviews.objects.filter(
+            author=self.context['request'].user,
+            title=title
+        ).exists():
+            raise serializers.ValidationError('Отзыв уже оставлен')
+        return data
+
+
+class CommentsSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comments"""
+
+    class Meta:
+        model = Comments
+        fields = (('id', 'text', 'author', 'created'))
+        read_only_fields = ('pub_date',)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,12 +1,15 @@
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from django.core.exceptions import PermissionDenied
 
-from reviews.models import Categories, Genres, Titles
-from .serializers import (CategoriesSerializer, GenresSerializer,
-                          TitlesSerializer)
+from reviews.models import Categories, Genres, Titles, Reviews, Comments
 from .generic import CreateListDeleteViewSet
+from .serializers import (CategoriesSerializer, GenresSerializer,
+                          TitlesSerializer, ReviewsSerializer,
+                          CommentsSerializer)
+from .permissions import OwnerOrReadOnly
 
 
 class CategoriesViewSet(CreateListDeleteViewSet):
@@ -38,3 +41,41 @@ class TitlesViewSet(viewsets.ModelViewSet):
         if self.request.user != serializer.instance.author:
             raise PermissionDenied("Изменения доступны только ?")
         return super().perform_update(serializer)
+
+
+class ReviewsViewSet(CreateListDeleteViewSet):
+    """Класс для работы с Отзывами"""
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = OwnerOrReadOnly
+
+    def get_title_id(self):
+        return get_object_or_404(Titles, pk=self.kwargs.get('title_id'))
+
+    def perform_create(self, serializer):
+        title = self.get_title_id()
+        serializer.save(author=self.request.user, title=title)
+
+    def get_queryset(self):
+        title = self.get_title_id()
+        return title.comments.all()
+
+
+class CommentsViewSet(CreateListDeleteViewSet):
+    """Класс для работы с Комментариями"""
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = OwnerOrReadOnly
+
+    def get_reviews_id(self):
+        return get_object_or_404(Reviews, pk=self.kwargs.get('reviews_id'))
+
+    def perform_create(self, serializer):
+        reviews = self.get_reviews_id()
+        serializer.save(author=self.request.user, reviews=reviews)
+
+    def get_queryset(self):
+        reviews = self.get_reviews_id()
+        return reviews.comments.all()

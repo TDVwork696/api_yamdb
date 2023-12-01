b58ff709d43e55922d830e8c50103f284feadcf1
Сергей Зайцев <seregame75@yandex.ru>
2023-12-01 22:44:28 +0300

    update umport csv ver3

--- api_yamdb/api/management/commands/import_csv_to_db.py
+++ api_yamdb/api/management/commands/import_csv_to_db.py
@@ -1,5 +1,5 @@
 import csv
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from reviews.models import Categories, Comments, Genres, Review, Title
 from user.models import CustomUser
@@ -19,17 +19,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for table in self.dict:
-            # Открываем файл таблицы
-            with open(f"../api_yamdb/static/data/{table}.csv") as file:
-                # Убираем первую строчку в которой название столбцов
-                # (id, name, slug и т.п.)
-                heading = next(file).rstrip().split(',')
-                reader = csv.reader(file)  # Читаем файл
-                model = self.dict[table]  # Получаем модель для таблицы
-                for row in reader:
-                    objs = {}
-                    for i in range(len(heading)):
-                        ele = heading[i]  # Получаем название стобца
-                        objs[ele] = row[i]
-                    # Создаем объект модели
-                    model.objects.update_or_create(**objs)
+            try:
+                # Открываем файл таблицы
+                with open(f"../api_yamdb/static/data/{table}.csv") as file:
+                    # Убираем первую строчку в которой название столбцов
+                    # (id, name, slug и т.п.)
+                    heading = next(file).rstrip().split(',')
+                    reader = csv.reader(file)  # Читаем файл
+                    model = self.dict[table]  # Получаем модель для таблицы
+                    for row in reader:
+                        objs = {}
+                        for i in range(len(heading)):
+                            ele = heading[i]  # Получаем название стобца
+                            objs[ele] = row[i]
+                        # Создаем объект модели
+                        model.objects.update_or_create(**objs)
+            except FileNotFoundError:
+                raise CommandError(f'File not found: {table}')
+            except Exception as e:
+                raise CommandError(f'Error processing file: {str(e)}')

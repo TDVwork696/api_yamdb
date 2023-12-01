a4bbdedb9341b17fa26ad25c7616e59dfe2f277d
Сергей Зайцев <seregame75@yandex.ru>
2023-12-01 22:25:03 +0300

    update umport csv

--- api_yamdb/api/management/commands/import_csv_to_db.py
+++ api_yamdb/api/management/commands/import_csv_to_db.py
@@ -4,80 +4,31 @@ from django.core.management.base import BaseCommand
 from reviews.models import Categories, Comments, Genres, Review, Title
 from user.models import CustomUser
 
-
 class Command(BaseCommand):
-    """Класс комажны выполняющей импорт данных из файла"""
     help = 'Import data from csv to db'
-    path_category = "../api_yamdb/static/data/category.csv"
-    path_comments = "../api_yamdb/static/data/comments.csv"
-    path_genre = "../api_yamdb/static/data/genre.csv"
-    path_genre_title = "../api_yamdb/static/data/genre_title.csv"
-    path_review = "../api_yamdb/static/data/review.csv"
-    path_titles = "../api_yamdb/static/data/titles.csv"
-    path_users = "../api_yamdb/static/data/users.csv"
+
+    dict = {
+        'users': CustomUser,
+        'genre': Genres,
+        'category': Categories,
+        'titles': Title,
+        'review': Review,
+        'comments': Comments,
+    }
 
     def handle(self, *args, **options):
-
-        with open(self.path_users) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                CustomUser.objects.get_or_create(
-                    id=row[0],
-                    username=row[1],
-                    email=row[2],
-                    role=row[3],
-                    bio=row[4],
-                    first_name=row[5],
-                    last_name=row[6]
-                )
-
-        with open(self.path_category) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                Categories.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2]
-                )
-
-        with open(self.path_genre) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                Genres.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2]
-                )
-
-        with open(self.path_titles) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                Title.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    year=row[2],
-                    category_id=row[3]
-                )
-
-        with open(self.path_review) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                Review.objects.get_or_create(
-                    id=row[0],
-                    title_id=row[1],
-                    text=row[2],
-                    author_id=row[3],
-                    score=row[4],
-                    pub_date=row[5]
-                )
-
-        with open(self.path_comments) as file:
-            reader = csv.reader(next(file))
-            for row in reader:
-                Comments.objects.get_or_create(
-                    id=row[0],
-                    review_id=row[1],
-                    text=row[2],
-                    author_id=row[3],
-                    pub_date=row[4]
-                )
+        # Перебираем словарь
+        for table in self.dict:
+            #открываем файл таблицы
+            with open(f"../api_yamdb/static/data/{table}.csv") as file:
+                 # Убираем первую строчку в которой название столбцов(id, name, slug и т.п.)
+                heading = next(file).rstrip().split(',')
+                reader = csv.reader(file) # Читаем файл
+                model = self.dict[table] # Получаем модель для таблицы
+                for row in reader: # Перебираем строки
+                    # Цикл длинной в количество стобцов
+                    objs = {} # Объявляем пустой словарь
+                    for i in range(len(heading)):
+                        ele = heading[i] # Получаем название стобца
+                        objs[ele] = row[i] # Добавляем в словарь значения из строки
+                    model.objects.update_or_create(**objs) # Создаем объект модели
